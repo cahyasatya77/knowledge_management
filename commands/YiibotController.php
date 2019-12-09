9dcf68e41bacf666af94271dfcd2498aa060ec76
fujitora <cahyasatya77@gmail.com>
2019-12-09 15:59:22 +0700

    menambah YiibotController join member baru

--- commands/YiibotController.php
+++ commands/YiibotController.php
@@ -22,14 +22,14 @@ class YiibotController extends Controller
         }
     }
     
-    protected function requesturt($method) {
+    protected function requesturl($method) {
         // $method = $method;
         return 'https://api.telegram.org/bot'.Yii::$app->params['token'].'/' . $method;
     }
     
     protected function getUpdates($offset)
     {
-        $url = $this->requesturt("getUpdates"). "?offset=" . $offset;
+        $url = $this->requesturl("getUpdates"). "?offset=" . $offset;
         $resp = file_get_contents($url);
         $result = json_decode($resp, true);
         if ($result['ok'] == 1) {
@@ -43,13 +43,13 @@ class YiibotController extends Controller
         $data = [
             'chat_id' => $chatid,
             'text' => $text,
-            'replay_to_message_id' => $msgid,
+            'reply_to_message_id' => $msgid,
         ];
         
         $client = new Client();
         $response = $client->createRequest()
                 ->setMethod('post')
-                ->setUrl($this->requesturt('sendMessage'))
+                ->setUrl($this->requesturl('sendMessage'))
                 ->addHeaders([
                     'content-type' => 'application/x-www-form-urlencoded',
                 ])
@@ -64,7 +64,7 @@ class YiibotController extends Controller
     
     protected function createResponse($text) 
     {
-        return "definisi ".$text;
+        return $text;
     }
     
     protected function processMessage($message) 
@@ -79,6 +79,16 @@ class YiibotController extends Controller
             $this->sendReplay($chatid, $message_id, $response);
         }
         
+        if (isset($message_data["new_chat_member"])) {
+            $chatid = $message_data["chat"]["id"];
+            $message_id = $message_data["message_id"];
+            $pesan = "Hai ".$message_data['new_chat_member']['first_name']."\n";
+            $pesan.= "Selamat datang di Grup ".$message_data['chat']['title']."\n";
+            $pesan.= "Silahkan perkenalkan diri anda.";
+            $response = $this->createResponse($pesan);
+            $this->sendReplay($chatid, $message_id, $response);
+        }
+        
         return $updateid;
     }
 
@@ -87,8 +97,12 @@ class YiibotController extends Controller
         $update_id = 0 ;
         
         if (file_exists("last_update_id")) {
-            $update_id = (int) file_get_contents("last_updete_id");
+            $update_id = (int) file_get_contents("last_update_id");
         }
+//        
+//        if (file_exists("new_chat_members")) {
+//            $update_id = file_get_contents("new_chat_members");
+//        }
         
         $updates = $this->getUpdates($update_id);
         
